0aa5e529fa2470e55f9f4fa206d1ea734d3e815a
pmorariu <morariupetru@yahoo.com>
2025-06-25 11:27:50 +0300

    Added 2 dummy products and created a home view to load the two products

--- onlinestore/views.py
+++ onlinestore/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+products = [
+    {
+        'name':"Car",
+        'description':"Description for the Car",
+        'price': 12.23,
+        'stock':20
+    },
+    {
+        'name': "Bike",
+        'description': "Description for the Bike",
+        'price': 2.23,
+        'stock': 25
+    }
+]
+
+def home(request):
+    context = {
+        'products': products
+    }
+    return render(request,'onlinestore/home.html',context)
