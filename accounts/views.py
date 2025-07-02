5af9ba443691f5a1ca684f90d1e1401af6aa23b9
pmorariu <morariupetru@yahoo.com>
2025-07-02 09:14:28 +0300

    Added login functionality for a created user as well as the one time meesage login in base.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import login
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def register_user(request):
     if request.method == 'POST':
@@ -14,3 +16,16 @@ def register_user(request):
     return render(request,'accounts/register.html',{'form':form})
 
 
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('store-home')
+        else:
+            messages.error(request, f'Please enter a correct username and password. Note that both fields may be case-sensitive.')
+            return redirect('login')
+    else:
+        form = AuthenticationForm()
+        return render(request,'accounts/login.html',{'form':form})
